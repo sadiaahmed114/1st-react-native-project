9b93d373c8320793d75f2da495d417ab98b8be65
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-11-10 10:02:54 -0800

    1st Basic Project

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import FlatCards from './components/FlatCards'
 import ElevatedCard from './components/ElevatedCard'
 import FancyCard from './components/FancyCard'
+import ActionCard from './components/ActionCard'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <FlatCards/>
         <ElevatedCard/>
         <FancyCard/>
+        <ActionCard/>
       </ScrollView>
     </SafeAreaView>
   )

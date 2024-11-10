1f3ca747d685b0ee440d0d35288d29e76d2b1502
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-11-10 05:11:45 -0800

    2nd basic project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
+import FlatCards from './components/FlatCards'
+import ElevatedCard from './components/ElevatedCard'
+import FancyCard from './components/FancyCard'
 
 const App = () => {
   return (
-    <View>
-      <Text>hey! what are you doing</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <FlatCards/>
+        <ElevatedCard/>
+        <FancyCard/>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 

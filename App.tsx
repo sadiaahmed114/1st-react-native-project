ea23f4601db683d3ae8a3ff5ca8831a67d3ecf5b
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-11-10 10:20:08 -0800

    1st Basic Project

--- App.tsx
+++ App.tsx
@@ -4,15 +4,19 @@ import FlatCards from './components/FlatCards'
 import ElevatedCard from './components/ElevatedCard'
 import FancyCard from './components/FancyCard'
 import ActionCard from './components/ActionCard'
+import ContactList from './components/ContactList'
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
+
         <FlatCards/>
         <ElevatedCard/>
         <FancyCard/>
         <ActionCard/>
+        <ContactList/>
+        
       </ScrollView>
     </SafeAreaView>
   )
--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -40,18 +40,57 @@ export default function ActionCard() {
 
 const styles = StyleSheet.create({
     headingText:{
-
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8,
     },
-    card:{},
-    elevatedCard:{},
-    headingContainer:{},
-    headerText:{},
-    cardImage:{
-        height:100
+    card: {
+        width: 320,
+        height: 360,
+        borderRadius: 6,
+        marginVertical: 12,
+        marginHorizontal: 16
     },
-    bodyContainer:{
-
+    elevatedCard: {
+        backgroundColor: '#E07C24',
+        elevation: 3,
+        shadowOffset: {
+            width: 1,
+            height: 1
+        },
+        shadowColor: '#333',
+        shadowOpacity: 0.4
     },
-    footerContainer:{},
-    socialLinks:{},
+    headingContainer: {
+        height: 40,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    headerText: {
+        color: '#000',
+        fontSize: 16,
+        fontWeight: '600'
+    },
+    cardImage: {
+        height: 190
+    },
+    bodyContainer: {
+        padding: 10
+    },
+    footerContainer: {
+        padding: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    },
+    socialLinks: {
+        fontSize: 16,
+        color: '#000000',
+        backgroundColor: '#FFF',
+        paddingHorizontal: 20,
+        paddingVertical: 6,
+        borderRadius: 6
+        
+    }
 })
